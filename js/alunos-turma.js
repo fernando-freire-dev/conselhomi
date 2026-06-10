7329dcae82acf64ef625520dcc3fc578720ec95b
Fernando H G Freire <vrfernandofreire@gmail.com>
2026-06-09 21:21:01 -0300

    Add page loading function for logged-in professor

---
+++ js/alunos-turma.js
@@ -0,0 +1,29 @@
+let professorLogado = null;
+
+async function carregarPagina() {
+
+  const {
+    data: { user }
+  } = await supabaseClient.auth.getUser();
+
+  if (!user) {
+    window.location.href = "index.html";
+    return;
+  }
+
+  const { data: profile } = await supabaseClient
+    .from("profiles")
+    .select("*")
+    .eq("id", user.id)
+    .single();
+
+  professorLogado = profile;
+
+  document.getElementById("tituloTurma").innerText =
+    `Professor: ${profile.nome}`;
+}
+
+document.addEventListener(
+  "DOMContentLoaded",
+  carregarPagina
+);
